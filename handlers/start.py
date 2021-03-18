88ca220cbd622ec56347a5f671844bed35d2fee8
Roj Serbest <rojserbest@gmail.com>
2021-03-18 09:57:05 +0300

    Initial commit

---
+++ handlers/start.py
@@ -0,0 +1,51 @@
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from helpers.filters import command, other_filters, other_filters2
+
+
+@Client.on_message(command("start") & other_filters)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
+
+I am SU Music Player, an open-source bot that lets you play music in your Telegram groups.
+
+Use the buttons below to know more about me.""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚒ Source code", url="https://t.me/SuMusicPlayer"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/su_Chats"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel 🔈", url="https://t.me/su_Bots"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(command("start") & other_filters2)
+async def start2(_, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
